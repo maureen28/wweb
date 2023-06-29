9a3f4800209ac3190eb81315206fdb03a16c4621
maureen <maureen.murimi@moringaschool.com>
2023-06-29 19:01:36 +0300

    Add Js

---
+++ class.js
@@ -0,0 +1,72 @@
+// // constant
+// 1, "hello"
+// // Assignment
+// Caro = precious
+// age = 20
+
+// // lookup
+// Caro
+// age
+
+// // Operators
+// + - add
+// * -multiply
+// / - division
+// - Subtraction
+// ** -Exponentiation
+// ++ increment
+// -- decrement
+// % -remainder
+// = assignment
+
+
+// // Variables
+
+// var 
+// let 'hello'
+// const x = 7
+
+// let x = 6
+// console.log(x)
+
+// DATA TYPES
+// //Numbers
+// // String
+// "hello"
+// 'world'
+// // Boolean
+// true
+// false
+// // Undefined
+// // Null
+
+// // objects
+
+// cara = {name : 'Ford'}
+// console.log(cara)
+// cars = [
+//     {name : 'Ford'}
+// ]
+
+
+// Array
+const x = [1, 3, 'hey', 'wassup', true, {name:'moh'}]
+const cars = ['Ford', 'Benz', 'prado','Jeep']
+
+ console.log(cars.slice(1)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
